aeb351b655057ef7b73f9f2b5ae91cd17c501d53
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-08 12:31:35 +0000

    Deploy IgorCIs/sudoque to github.com/IgorCIs/sudoque.git:gh-pages

---
+++ precache-manifest.11f1ab809698ee67e3ac95ac20141710.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "40b0e45c9a13b24d4f809c65ecfb09fb",
+    "url": "/static/media/undo.40b0e45c.svg"
+  },
+  {
+    "revision": "f91726e046e72ce034da1709eb0e037c",
+    "url": "/static/media/light-bulb.f91726e0.svg"
+  },
+  {
+    "revision": "be6feb2412a9c222fdf17a9117ba0169",
+    "url": "/static/media/eraser.be6feb24.svg"
+  },
+  {
+    "revision": "7d0dcef1888c10a7ea4763eefbbe049e",
+    "url": "/static/media/edit.7d0dcef1.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "25bffd5de811eac39c84",
+    "url": "/static/js/main.25bffd5d.chunk.js"
+  },
+  {
+    "revision": "bc28a9dff999a095abcb",
+    "url": "/static/js/1.bc28a9df.chunk.js"
+  },
+  {
+    "revision": "25bffd5de811eac39c84",
+    "url": "/static/css/main.6acee69f.chunk.css"
+  },
+  {
+    "revision": "7137031b44077ba544f2b56bf112e809",
+    "url": "/index.html"
+  }
+];
